e7c272f264faf738dfc11145c711e8bf07253f8c
Jose Avalos <avaljose@hotmai.com>
2021-11-16 22:36:22 -0600

    working on views

--- 02-Homework/db/schema.sql
+++ 02-Homework/db/schema.sql
@@ -3,12 +3,12 @@ DROP DATABASE IF EXISTS employueeTrackerDB;
 CREATE DATABASE employueeTrackerDB;
 
 USE employueeTrackerDB
-
+-- department table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     NAME VARCHAR(30)
 );
-
+-- Role table           
 CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
@@ -16,9 +16,9 @@ CREATE TABLE role (
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
-
+-- Employee table
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,~
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     manager_id INT,
@@ -26,3 +26,4 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
+
---
+++ 02-Homework/db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department(department_name)
+VALUES("Engineering"),
+ ("Sales"),
+ ("Finance"),
+ ("Legal"),
+ ("Marketing");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("Engineer", 85000, 1),
+("Senior Engineer", 125000, 1),
+("CFO", 350000, 3),
+("Chief Counsel", 300000, 4),
+('Marketing Coordindator', 70000, 3), 
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('Mark', 'Miller', 2, null),
+('Devin', 'Anderson', 1, 1),
+('Mary', 'Brown', 4, null),
+('Ashley', 'Jones', 3, 3),
+('Tyler', 'Moore', 6, null),
+('Ana', 'Sanchez', 5, 5),
+('Lewis', 'Allen', 7, null),
+('Katherine', 'Green', 8, 7);
